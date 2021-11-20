50cc348ae362c5629f45c10d010a1b8cd321ffba
Spaincraft <93721517+Spaincraft@users.noreply.github.com>
2021-11-20 16:31:36 +0000

    Add files via upload

---
+++ Julia.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+bins=100
+c1 = 0
+x1 = np.linspace(-2,2,bins)
+
+
+c2= 0.21
+x2 = np.linspace(-2,2,bins)
+
+def JULIA(x,c,ite):  
+    n = (len(x))
+    out = np.zeros((n,n))
+    for i in range(n):
+        for j in range(n):
+            m = 0
+            c = complex(x[i],x[j])
+            z = 0
+            for k in range(ite):
+                z = z*z + c
+                m += 1
+                if (abs(z)>4):
+                    break
+            out[i,j] = m
+    return (out)
+
+
+def JULIAEXP(x,c,ite):  
+    n = (len(x))
+    out = np.zeros((n,n))
+    for i in range(n):
+        for j in range(n):
+            m = 0
+            c = complex(x[i],x[j])
+            z = 0
+            for k in range(ite):
+                z =(np.exp(z))*(z**3) + c
+                m += 1
+                if (abs(z)>4):
+                    break
+            out[i,j] = m
+    return (out)
+
+
+
+b1 = JULIA(x1,c1,bins)
+plt.imshow(b1,cmap="jet")
+
+b2 = JULIAEXP(x2,c2,bins)
+plt.imshow(b2,cmap="jet")
+
+
+
+    
+
+
+
